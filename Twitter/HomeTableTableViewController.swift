c5f7e8194a90cd69817bbd21e57cdb6e29586be3
Arul Srivastava <71412314+arul-sri@users.noreply.github.com>
2020-10-19 15:51:27 -0400

    Added like and retweet to twitter

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -13,111 +13,124 @@ class HomeTableTableViewController: UITableViewController {
     
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
-    
-    let myRefeshControl = UIRefreshControl()
-    
+    let myRefreshControl = UIRefreshControl()
     override func viewDidLoad() {
-        super.viewDidLoad()
-        numberOfTweet = 20
-        myRefeshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        self.tableView.refreshControl = myRefeshControl
-    }
-    
-    
-    @objc func loadTweets(){
-        
-        numberOfTweet = 20
-        
-        
-        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": numberOfTweet]
-        
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
-            
-            self.tweetArray.removeAll()
-            for tweet in tweets {
-                self.tweetArray.append(tweet)
+    super.viewDidLoad()
+                myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+                tableView.refreshControl = myRefreshControl
+    self.loadTweet()
+    self.tableView.rowHeight = UITableView.automaticDimension
+    self.tableView.estimatedRowHeight = 190
             }
-            
-            self.tableView.reloadData()
-            self.myRefeshControl.endRefreshing()
-            
-            
-        }, failure: { (Error) in
-            print("Could not retrieve tweets! Oh no!")
-        })
-        
-    }
-    
-    
-    func loadMoreTweets(){
-        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        numberOfTweet = numberOfTweet + 20
-        let myParams = ["count": numberOfTweet]
-        
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
-            
-            self.tweetArray.removeAll()
-            for tweet in tweets {
-                self.tweetArray.append(tweet)
+    override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    self.loadTweet()
             }
-            
-            self.tableView.reloadData()
-            self.myRefeshControl.endRefreshing()
-            
-            
-        }, failure: { (Error) in
-            print("Could not retrieve tweets! Oh no!")
-        })
-        
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row + 1 == tweetArray.count {
-            loadMoreTweets()
+    @objc func loadTweet(){
+            numberOfTweet = 20
+    let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+    let myParams = ["count": numberOfTweet]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl , parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
+    self.tweetArray.removeAll()
+    for tweet in tweets {
+    self.tweetArray.append(tweet)
+    self.myRefreshControl.endRefreshing()
+                }
+    self.tableView.reloadData()
+            }, failure: {(Error) in
+    print("couldnt retrive tweet")
+                       })
         }
-    }
-
+    func loadMoreTweets(){
+    let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+            numberOfTweet = numberOfTweet + 20
+    let myParams = ["count": numberOfTweet]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl , parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
+    self.tweetArray.removeAll()
+    for tweet in tweets {
+    self.tweetArray.append(tweet)
+                }
+    self.tableView.reloadData()
+            }, failure: {(Error) in
+    print("couldnt retrive tweet")
+                       })
+        }
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {         if indexPath.row + 1 == tweetArray.count{
+                        loadMoreTweets()
+                        }
+            }
     @IBAction func onLogout(_ sender: Any) {
-        TwitterAPICaller.client?.logout()
-        self.dismiss(animated: true, completion: nil)
-        UserDefaults.standard.set(false, forKey: "userLoggedIn")
-    }
-    
-    
+            TwitterAPICaller.client?.logout()
+    self.dismiss(animated: true, completion: nil)
+            UserDefaults.standard.set(false, forKey: "userLoggedIn")
+        }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
-        
-        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
-
+    let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+    let user = tweetArray[indexPath.row]["user"] as! NSDictionary
         cell.userNameLabel.text = user["name"] as? String
         cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
-        
-        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
-        let data = try? Data(contentsOf: imageUrl!)
-        
-        if let imageData = data {
-            cell.profileImageView.image = UIImage(data: imageData)
+    let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+    let data = try? Data(contentsOf: imageUrl!)
+    if let imageData = data {
+                cell.profileImageView.image = UIImage(data: imageData)
+            }
+            cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+            cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+            cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+    return cell
         }
-        
-        return cell
-    }
-    
-    
-    
-    
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
+    // #warning Incomplete implementation, return the number of sections
+            return 1
+        }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return tweetArray.count
+    // #warning Incomplete implementation, return the number of rows
+            return tweetArray.count
+        }
+    /*
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+            // Configure the cell...
+            return cell
+        }
+        */
+    /*
+        // Override to support conditional editing of the table view.
+        override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+            // Return false if you do not want the specified item to be editable.
+            return true
+        }
+        */
+    /*
+        // Override to support editing the table view.
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                // Delete the row from the data source
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } else if editingStyle == .insert {
+                // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            }
+        }
+        */
+    /*
+        // Override to support rearranging the table view.
+        override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        }
+        */
+    /*
+        // Override to support conditional rearranging of the table view.
+        override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+            // Return false if you do not want the item to be re-orderable.
+            return true
+        }
+        */
+    /*
+        // MARK: - Navigation
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
     }
-
-
-}
--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -9,7 +9,7 @@
 import UIKit
 import BDBOAuth1Manager
 
-class TwitterAPICaller: BDBOAuth1SessionManager {    
+class TwitterAPICaller: BDBOAuth1SessionManager {
     static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "uFTmFW66AAMEUwx3rZlZDMSCf", consumerSecret: "LtlxIoQpBvHcqjpSMIA9Gs2E9wCJbr7xkx9EpSdBYoNedaZUgh")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
@@ -69,4 +69,27 @@ class TwitterAPICaller: BDBOAuth1SessionManager {
         TwitterAPICaller.client?.post(url, parameters: ["status": tweetString], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in success()}, failure: { (task: URLSessionDataTask?, error: Error) in failure(error)
         })
     }
+    
+    func favotiteTweet(tweetId:Int, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+        let url = "https://api.twitter.com/1.1/favorites/create.json"
+        TwitterAPICaller.client?.post(url, parameters: ["id": tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in success()}, failure: { (task: URLSessionDataTask?, error: Error) in failure(error)
+        })
+    }
+    
+    func unfavoutiteTweet(tweetId:Int, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+        let url = "https://api.twitter.com/1.1/favorites/destroy.json"
+        TwitterAPICaller.client?.post(url, parameters: ["id": tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in success()}, failure: { (task: URLSessionDataTask?, error: Error) in failure(error)
+        })
+    }
+    func retweet(tweetId: Int, success: @escaping () -> (), failure: @escaping (Error) -> ()) {
+        
+        let url = "https://api.twitter.com/1.1/statuses/retweet/\(tweetId).json"
+        TwitterAPICaller.client?.post(url, parameters: ["id": tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+                success()
+            }, failure: { (task: URLSessionDataTask?, error: Error) in
+                failure(error)
+            })
+    }
+    
+    
 }
